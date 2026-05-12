29b086fe063a9f0ae35c8a943273f99c3dd183ac
owen101russell <104833880+owen101russell@users.noreply.github.com>
2026-05-12 11:23:49 +0200

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+export const supabase = createClient(
+  'https://vfsxjqtfounzvygyzbmz.supabase.co',
+  'sb_publishable_VfYeAak7___vO2Dxw0SotA_cjWqO8cb'
+)
