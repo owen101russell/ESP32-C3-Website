d4f5b34694cb120632bb4673ed712c0b30695db1
owen101russell <104833880+owen101russell@users.noreply.github.com>
2026-05-12 11:26:05 +0200

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-export const supabase = createClient(
-  'https://vfsxjqtfounzvygyzbmz.supabase.co',
-  'sb_publishable_VfYeAak7___vO2Dxw0SotA_cjWqO8cb'
-)
